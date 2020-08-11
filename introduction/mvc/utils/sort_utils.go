011069d793daea4c9a1477c01596ec576b3e9586
Kuan-Chou Chen <kuan-chouchen@Kuan-Chous-MBP.home>
2020-08-10 20:07:59 -0700

    Add OptimalSort function to take advantages from both

--- introduction/mvc/utils/sort_utils.go
+++ introduction/mvc/utils/sort_utils.go
@@ -1,5 +1,7 @@
 package utils
 
+import "sort"
+
 func BubbleSort(elements []int) {
 	keepRunning := true
 	for keepRunning {
@@ -10,5 +12,16 @@ func BubbleSort(elements []int) {
 				keepRunning = true
 			}
 		}
+	}
+}
+
+func OptimalSort(elements []int) {
+	// after testing with benchmark, we figured that 30000 is kind of a threshold, that being said
+	// if there are less than 30K elements, using bubble sort is more efficient, otherwise using Go built-in sort function
+	// is more efficient
+	if len(elements) < 30000 {
+		BubbleSort(elements)
+	} else {
+		sort.Ints(elements)
 	}
 }
